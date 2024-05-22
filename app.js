4d51ef51eed6897158ecadeddaab5919a478ab12
joni <codingmylife0@gmail.com>
2024-05-22 22:04:32 +0530

    update

--- app.js
+++ app.js
@@ -3,39 +3,43 @@ let userInput = document.querySelector("#user-input");
 let result = document.querySelector("#result");
 let stargameBtn = document.querySelector("#star-game");
 let submitBtn = document.querySelector("#submit")
-let allguesArry = []
-let randomnum = Math.floor(Math.random() * 100);
 let guesNumber = document.querySelector("#your-gues");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let userInputValue = parseInt(userInput.value);
-    console.log(userInputValue);
-
-    if (userInputValue > randomnum) {
-
-        result.innerHTML = " too hight !!"
-    } else if (userInputValue < randomnum) {
-
-        result.innerHTML = " too low !!"
-    } else {
-
-        result.innerHTML = " congra !!"
-        stargameBtn.disabled = false;
-        submitBtn.disabled = true;
-    }
-
-    allguesArry.push(userInputValue)
-
-    guesNumber.innerHTML = " your gues" + "  " + allguesArry.join(",")
-    // console.log("form submit");
-    form.reset()
-    stargameBtn.addEventListener("click", () => {
-        result.innerHTML = "";
-        guesNumber.innerHTML = "";
-        stargameBtn.disabled = true;
-        submitBtn.disabled = false;
-        let randomnum = Math.floor(Math.random() * 100);
-    })
-});
+(function(){
+    let allguesArry = []
+    let randomnum = Math.floor(Math.random() * 100);
+    
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        let userInputValue = parseInt(userInput.value);
+        console.log(userInputValue);
+    
+        if (userInputValue > randomnum) {
+    
+            result.innerHTML = " too hight !!"
+        } else if (userInputValue < randomnum) {
+    
+            result.innerHTML = " too low !!"
+        } else {
+    
+            result.innerHTML = " congra !!"
+            stargameBtn.disabled = false;
+            submitBtn.disabled = true;
+        }
+    
+        allguesArry.push(userInputValue)
+    
+        guesNumber.innerHTML = " your gues" + "  " + allguesArry.join(",")
+        // console.log("form submit");
+        form.reset()
+        stargameBtn.addEventListener("click", () => {
+            result.innerHTML = "";
+            guesNumber.innerHTML = "";
+            stargameBtn.disabled = true;
+            submitBtn.disabled = false;
+            let randomnum = Math.floor(Math.random() * 100);
+        })
+    });
+}
+)
